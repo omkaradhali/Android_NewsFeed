b7a2aedd1b499f70a2e1be605b6f2d820c99f5ab
omkaradhali <omkar.a2989@gmail.com>
2017-03-04 21:48:03 -0500

    First Commit

---
+++ NewsFeeds/app/src/main/java/oadhali/com/newsfeeds/MainActivity.java
@@ -0,0 +1,29 @@
+package oadhali.com.newsfeeds;
+
+import android.os.AsyncTask;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    
+
+    //<String1,void,String2> String1 = URL; void = progress bar; String2 = result
+    private class DownloadData extends AsyncTask<String , Void, String>{
+
+        @Override
+        protected String doInBackground(String... params) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
+    }
+}
